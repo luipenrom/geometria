e89ae55d251ff54b6a4a8debb302c1bc333e6bc3
@luipenrom <luipenrom@alu.edu.gva.es>
2022-02-12 20:44:51 +0100

    Version_Refactorizada

--- geometria/src/Circulo_LPR.java
+++ geometria/src/Circulo_LPR.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_LPR extends FiguraGeometrica_LPR {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_LPR(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_LPR.java
+++ geometria/src/FiguraGeometrica_LPR.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_LPR {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_LPR(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
